461e98bec37ffea8677ad9e5ddd39ee22ad4db5a
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-25 16:40:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5] + params[:word5]
+    @word = params[:word1] + "\n" + params[:word2] + "\n" + params[:word3] + "\n" + params[:word4] + "\n" + params[:word5]
     "#{@word}"
   end
 
