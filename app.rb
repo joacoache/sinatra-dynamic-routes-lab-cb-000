5d5680800a85415ba0638ea89ae33574c6b54222
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-25 16:48:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,10 +36,9 @@ class App < Sinatra::Base
     elsif params[:operation] == 'multiply'
       "#{params[:number1].to_i * params[:number2].to_i}"
     elsif params[:operation] == 'divide' && params[:number2] == 0
-      "#{params[:number1].to_i / params[:number2].to_i}"
+        "ZeroDivisionError"
     elsif params[:operation] == 'divide'
       "#{params[:number1].to_i / params[:number2].to_i}"
-      "ZeroDivisionError"
     else
       "invalid operation"
     end
