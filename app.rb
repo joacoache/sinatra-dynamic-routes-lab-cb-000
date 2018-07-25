57542aeab4624f61e31beee8c21c466d9e7ede35
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-25 16:44:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,6 +31,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     if params[:operation] == 'add'
       "#{params[:number1].to_i + params[:number2].to_i}"
+    end
   end
 
 end
